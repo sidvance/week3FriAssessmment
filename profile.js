9dd88e507d87da7dbe4bf2c5bffd0c1817338ab9
Cassidy <cassidy.vance@hotmail.com>
2023-03-19 02:22:27 -0700

    functioning buttons

---
+++ profile.js
@@ -0,0 +1,35 @@
+let color = document.querySelector('#color')
+
+const faveColor = () => {
+    alert('My fave color is lilac! Thanks for asking!!!')
+}
+
+color.addEventListener('click', faveColor)
+
+
+let place = document.querySelector('#place')
+
+const favePlace = () => {
+    alert('My favorite place in the world is Yosemite<3333')
+}
+
+place.addEventListener('click', favePlace)
+
+
+let ritual = document.querySelector('#ritual')
+
+const faveRitual = () => {
+    alert('My favorite ritual is laying under my weighted blanket with a book and waking up 4 hours later :\')')
+}
+
+ritual.addEventListener('click', faveRitual)
+
+
+let submitButton = document.querySelector('#submitBtn')
+
+const hiring = () => {
+    alert('This was a test. If you are not the Queen of Genovia, don\'t call again.')
+}
+
+submitButton.addEventListener('click', hiring)
+
